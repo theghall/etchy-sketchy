d74ad83aae0e852b4412c837b04a32425373513f
theghall <guategary@gmail.com>
2017-01-03 22:46:05 -0500

    Add sketch functionality

--- js/etchy-sketchy.js
+++ js/etchy-sketchy.js
@@ -2,36 +2,50 @@
 //
 // This file does the bulk of the work to display and interact with the grid
 //
-// 2017=01=03 GH  Created grid
+// 2017=01=03 GH  Created grid, mouseenter function
 //
+var defaultSize = 16; // Grid is even so this is both number of rows and cols
+var currentSize = defaultSize;
+var squareClass = "grid-square"
+
 $(document).ready(function() {
   initGrid();
+
+  // "draws" black line as mouse is dragged
+  $('div.grid-square').mouseenter(function() {
+      $(this).css({backgroundColor: "#000000"});
+  });
 });
+
 
 var initGrid = function() {
 
   // This is where the grid will be inserted
   var $gc = $("#grid-container");
 
-  var rowId = ""; // For the id to be added to the first DIV in each row
-  var divElem=""; // Used to build the DIV element
+  var rowId = "";   // For the id to be added to the first DIV in each row
+  var divElem = ""; // Used to build the DIV element
 
-  // Create 16 x 16 grid
-  for (var i = 1; i <=16; i++) {
+  // Create grid currentSize x currentSize
+  for (var i = 1; i <= currentSize; i++) {
 
       rowId = "R" + i;
 
-      for (var j = 1; j <=16; j++) {
+      for (var j = 1; j <= currentSize; j++) {
 
         // Put an id in first div of each row for debugging
         if (j === 1) {
-          divElem = '<div class="grid-row" ' + 'id="' + rowId + '">' + '</div>';
+          divElem = '<div class="' + squareClass + '" id="' + rowId + '" ></div>';
         }
         else {
-          divElem =  '<div class="grid-row"></div>';
+          divElem =  '<div class="' + squareClass + '"></div>';
         }
 
         $gc.append(divElem);
       }
   }
+}
+
+function clearGrid() {
+
 }
