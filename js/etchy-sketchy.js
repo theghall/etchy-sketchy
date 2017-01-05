4c8e43d67feb9379376bea0a3922a32cdd00d435
theghall <guategary@gmail.com>
2017-01-05 12:25:20 -0500

    Add rand color to mouseenter event

--- js/etchy-sketchy.js
+++ js/etchy-sketchy.js
@@ -4,6 +4,7 @@
 //
 // 2017-01-03 GH  Created grid, mouseenter function
 // 2017-01-04 GH  Added Clear Grid functionality
+// 2017-01-05 GH  Added rand color to mouseenter event
 //
 var defaultSize = 16; // Grid is even so this is both number of rows and cols
 var currentSize = defaultSize;
@@ -23,7 +24,7 @@ $(document).ready(function() {
 
 // initGrid appends DIV elements with the class squareClass which will be the
 // sqaures in our grid.  The grid will be gridSize x gridSize
-var initGrid = function(gridSize) {
+function initGrid(gridSize) {
 
   currentSize = gridSize;
 
@@ -40,14 +41,14 @@ var initGrid = function(gridSize) {
   var divElem = ""; // Used to build the DIV element
 
   // Create grid currentSize x currentSize
-  for (var i = 1; i <= currentSize; i++) {
+  for (var row = 1; row <= currentSize; row++) {
 
-      rowId = "R" + i;
+      rowId = "R" + row;
 
-      for (var j = 1; j <= currentSize; j++) {
+      for (var col = 1; col <= currentSize; col++) {
 
         // Put an id in first div of each row for debugging
-        if (j === 1) {
+        if (col === 1) {
           divElem = '<div class="' + squareClass + '" id="' + rowId + '\" ' + styleString + '></div>';
         }
         else {
@@ -59,11 +60,47 @@ var initGrid = function(gridSize) {
   }
 }
 
+var randRGB = function() {
+  return Math.floor(Math.random() * 255);
+}
+
+var randColor = function(currColor) {
+  var red, green, blue;
+  var blackDiff = [0, 0, 0];
+  var newColor = currColor;
+
+  var rgb = currColor.slice(4,currColor.indexOf(")")).split(",");
+
+  red = parseInt(rgb[0],10);
+  green = parseInt(rgb[1],10);
+  blue = parseInt(rgb[1], 10);
+
+  // If white gen random color, else move toward black by 10% until its black
+  if (red === 255 && green === 255 && blue === 255) {
+    newColor = "rgb(" + randRGB() + ", " + randRGB() + ", " + randRGB() + ")"
+  }
+  else if ( red !== 0 || green !== 0 || blue !== 0){
+    red = red - Math.floor(red * 0.10);
+    green = green - Math.floor(green * 0.10);
+    blue = blue = Math.floor(blue * 0.10);
+
+    if (red < 0) { red = 0; }
+    if (green < 0) { green = 0; }
+    if (blue < 0) {blue = 0; }
+
+    newColor = "rgb(" + red + ", " + green + ", " + blue + ")"
+
+  }
+
+  return newColor;
+};
+
 function initEvents() {
   // "draws" black line as mouse is dragged
   var targetElem = "div."+ squareClass;
   $(targetElem).mouseenter(function() {
-      $(this).css({backgroundColor: "#000000"});
+      var backgroundColor = $(this).css("background-color");
+      $(this).css({"background-color": randColor(backgroundColor)});
   });
 }
 
